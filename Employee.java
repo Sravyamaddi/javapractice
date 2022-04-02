a90a2253a3d189c328deec349161089f9feb14fe
Sravyamaddi <102852566+Sravyamaddi@users.noreply.github.com>
2022-04-02 18:02:37 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,53 @@
+package practice;
+
+public class Employee {
+
+	private String name;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	private String department;
+	
+	public Employee(String name, String department) {
+		this.name = name;
+		this.department = department;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", department=" + department + "]";
+	}
+	
+	
+}
