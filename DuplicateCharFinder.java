5cd5e41de082edc90e18e43cd9352cfad4dff2df
Sravyamaddi <102852566+Sravyamaddi@users.noreply.github.com>
2022-04-02 17:52:57 +0530

    Add files via upload

---
+++ DuplicateCharFinder.java
@@ -0,0 +1,41 @@
+package com.javapractice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class DuplicateCharFinder {
+
+	
+	public static void duplicateFinder(){
+		Map<Character , Integer> map = new HashMap<>();
+		
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+		
+		for(char c: str.trim().toCharArray()){
+			
+			if(map.containsKey(c)){
+				map.put(c,map.get(c)+1);
+			}else{
+				map.put(c, 1);
+			}
+			
+		}
+		
+		for(Entry<Character, Integer> m :map.entrySet() ){
+			
+			if(m.getValue() > 1){
+				System.out.println(m.getKey().toString() + "-" + m.getValue() + "times");
+			}
+		}
+		System.out.println(str);
+	}
+	
+	public static void main(String args[]){
+		
+		duplicateFinder();
+	}
+	
+}
