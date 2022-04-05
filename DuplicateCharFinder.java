905546d8e4671f90350a7758ff7e964c413a8b5b
Sravyamaddi <102852566+Sravyamaddi@users.noreply.github.com>
2022-04-05 14:25:23 +0530

    Update DuplicateCharFinder.java

--- DuplicateCharFinder.java
+++ DuplicateCharFinder.java
@@ -10,31 +10,32 @@ public class DuplicateCharFinder {
 	
 	public static void duplicateFinder(){
 		Map<Character , Integer> map = new HashMap<>();
-		
 		Scanner s = new Scanner(System.in);
 		String str = s.nextLine();
-		
-		for(char c: str.trim().toCharArray()){
-			
-			if(map.containsKey(c)){
-				map.put(c,map.get(c)+1);
-			}else{
-				map.put(c, 1);
+		try {
+			for(char c: str.trim().toCharArray()){
+				if(map.containsKey(c)){
+					map.put(c,map.get(c)+1);
+				}else{
+					map.put(c, 1);
+				}
 			}
-			
-		}
-		
-		for(Entry<Character, Integer> m :map.entrySet() ){
-			
-			if(m.getValue() > 1){
-				System.out.println(m.getKey().toString() + "-" + m.getValue() + "times");
+			for(Entry<Character, Integer> m :map.entrySet() ){
+				if(m.getValue() > 1){
+					System.out.println(m.getKey().toString() + "-" + m.getValue() + "times");
+				}
 			}
+			//System.out.println(str);
 		}
-		System.out.println(str);
+		catch(Exception e) {
+			System.err.printf("Exception in finding duplicate word"+str ,e);
+		}
+		finally {
+			s.close();
+		}
 	}
 	
 	public static void main(String args[]){
-		
 		duplicateFinder();
 	}
 	
