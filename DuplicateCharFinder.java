4aaf75dd4c4f889ee359dc1cf5005b8c2f92db33
Sravyamaddi <102852566+Sravyamaddi@users.noreply.github.com>
2022-04-08 16:31:19 +0530

    Update DuplicateCharFinder.java

--- DuplicateCharFinder.java
+++ DuplicateCharFinder.java
@@ -7,36 +7,34 @@ import java.util.Scanner;
 
 public class DuplicateCharFinder {
 
-	
-	public static void duplicateFinder(){
-		Map<Character , Integer> map = new HashMap<>();
-		Scanner s = new Scanner(System.in);
-		String str = s.nextLine();
+	public static void main(String args[]) {
+		
+		Map<Character, Integer> map = new HashMap<>();
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		Boolean repeatedChar = false;
 		try {
-			for(char c: str.trim().toCharArray()){
-				if(map.containsKey(c)){
-					map.put(c,map.get(c)+1);
-				}else{
-					map.put(c, 1);
+			for (char letter : str.trim().toCharArray()) {
+				if (map.containsKey(letter)) {
+					map.put(letter, map.get(letter) + 1);
+				} else {
+					map.put(letter, 1);
 				}
 			}
-			for(Entry<Character, Integer> m :map.entrySet() ){
-				if(m.getValue() > 1){
+			for (Entry<Character, Integer> m : map.entrySet()) {
+				if (m.getValue() > 1) {
 					System.out.println(m.getKey().toString() + "-" + m.getValue() + "times");
+					repeatedChar = true;
 				}
 			}
-			//System.out.println(str);
-		}
-		catch(Exception e) {
-			System.err.printf("Exception in finding duplicate word"+str ,e);
-		}
-		finally {
-			s.close();
+			if (!repeatedChar) {
+				System.out.println("No repeated characters in given word");
+			}
+		} catch (Exception e) {
+			System.err.printf("Exception in finding duplicate word" + str, e);
+		} finally {
+			scanner.close();
 		}
 	}
-	
-	public static void main(String args[]){
-		duplicateFinder();
-	}
-	
+
 }
